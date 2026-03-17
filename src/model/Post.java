1c311d04bca950a59a9e6d4f23f76f3c283b6c7d
Ritvars <ritvarsbaders@gmail.com>
2026-03-17 11:43:35 +0200

    Edit: added get and setter to post class also addedconstructor and toString

--- src/model/Post.java
+++ src/model/Post.java
@@ -1,5 +1,42 @@
 package model;
 
-public class Post {
+import java.time.LocalDateTime;
 
+public class Post {
+	private String msg;
+	private LocalDateTime dateTime = LocalDateTime.now();
+	private int countOfLikes = 0;
+	
+	
+	public String getMsg() {
+		return msg;
+	}
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+	public int getCountOfLikes() {
+		return countOfLikes;
+	}
+	
+	public void setMsg(String newMsg) {
+		msg = newMsg;
+	}
+	public void getLike() {
+		countOfLikes++;
+	}
+
+	Post(String newMsg){
+		if((newMsg != null) && (!newMsg.isEmpty()) && (newMsg.length() < 1000)) {
+			msg = newMsg;
+		}
+		else {
+			msg = "Unknown post";
+		}
+	}
+	
+	public String toString() {
+		String result = msg + " | " + dateTime + " | " + countOfLikes;
+		return result;
+	}
+	
 }
